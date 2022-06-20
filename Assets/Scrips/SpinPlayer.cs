525f80ea0be6b17790f88f43456d778ba8f51fab
nZhiep <nguyennghiep652000@gmail.com>
2022-06-20 20:40:37 +0700

    Update1 fix file

--- Assets/Scrips/SpinPlayer.cs
+++ Assets/Scrips/SpinPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpinPlayer : MonoBehaviour
 {
@@ -8,48 +9,93 @@ public class SpinPlayer : MonoBehaviour
     //public GameObject Player;
     Vector3 Vtri0;
     private bool quick;
+    private bool clickQup;
+    private bool clickQdown;
+
+    int i = 1;
+    public Button Quickbtn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         Vtri0 = OngNgam.transform.position;
-        if(quick)
+        if (clickQdown)
         {
-            clickDown();
+            toDown();
         }
+        if (clickQup)
+        {
+            toUp();
+        }
+
         //setDC();
     }
     public void clickDown()
     {
-        
-        //Vector3 Vtri0 = OngNgam.transform.position;
-        Vector3 dir = Vtri0 - transform.position;
-        dir.Normalize();
-        float vecto = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, vecto );
-        Debug.Log(vecto);
+        if (quick == true)
+        {
+            clickQdown = true;
+        }
+        {
+            toDown();
+        }
+
+
     }
     public void clickUp()
     {
-
+        if (quick == true)
+        {
+            clickQup = true;
+        }
+        else
+        {
+            toUp();
+        }
+        
+    }
+    public void UnClickUp()
+    {
+        clickQup = false;
+    }
+    public void UnClickDown()
+    {
+        clickQdown = false;
+    }
+    public void ClickQuick()
+    {
+        i++;
+        if(i %2 == 0)
+        {
+            Quickbtn.image.color = new Color32(250,250,250,250);
+            quick = true;
+        }
+        else
+        {
+            Quickbtn.image.color = new Color32(250, 250, 250, 36);
+            quick = false;
+        }
+    }
+    public void toDown()
+    {
         Vector3 dir = Vtri0 - transform.position;
         dir.Normalize();
-        float vecto = 1.1f+(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+        float vecto = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, vecto);
-        Quaternion.Inverse(transform.rotation);
         //Debug.Log(vecto);
     }
-    public void SpinQuick()
+    public void toUp()
     {
-        quick = true;
-    }
-    public void UnSpinQuick()
-    {
-        quick = false;
+        Vector3 dir = Vtri0 - transform.position;
+        dir.Normalize();
+        float vecto = 1.1f + (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+        transform.rotation = Quaternion.Euler(0f, 0f, vecto);
+        Quaternion.Inverse(transform.rotation);
     }
 }
